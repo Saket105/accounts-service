3ef7b80229ed853b0006eb366a514c7355316e13
saket105 <saketbsc123@gmail.com>
2025-09-06 13:49:21 +0530

    entity and repository done

---
+++ src/main/java/com/saket/eazybytes/accountsservice/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.saket.eazybytes.accountsservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @Column(updatable = false)
+    private String createdBy;
+    @Column(insertable = false)
+    private LocalDateTime updateAt;
+    @Column(insertable = false)
+    private String updateBy;
+}
